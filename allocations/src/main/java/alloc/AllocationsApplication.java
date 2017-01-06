4d614688aeaa5b268c5e31a5200e52c9a5c1967b
Sudarsana <sudaredd@gmail.com>
2017-01-06 14:51:00 -0500

    initial

---
+++ allocations/src/main/java/alloc/AllocationsApplication.java
@@ -0,0 +1,12 @@
+package alloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocationsApplication.class, args);
+	}
+}
