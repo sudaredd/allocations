6d4121d1ff7ca34dfa8ae76ce05630ac1259b29f
Sudarsana <sudaredd@gmail.com>
2017-01-06 15:41:48 -0500

    test dependencies

--- allocations/src/test/java/alloc/AllocationsApplicationTests.java
+++ allocations/src/test/java/alloc/AllocationsApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=AllocationsApplication.class)
 public class AllocationsApplicationTests {
 
 	@Test
